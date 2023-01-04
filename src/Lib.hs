df3bf6e998af2635a98912c841a1b7d9e3dcbb0d
Dario <dario.vidas92@gmail.com>
2023-01-03 16:13:10 -0800

    Make it work with gloss

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,15 @@ module Lib
     ( someFunc
     ) where
 
+import Control.Concurrent (threadDelay)
+import Control.Monad (forever)
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+-- Call the function every 1 second
+loopFn :: IO () -> IO ()
+loopFn fn = forever $ do
+    fn
+    threadDelay 1000000
+
