ea373ac8e11e0a7caf405818feb534a13eae39df
Dario <dario.vidas92@gmail.com>
2023-01-04 21:26:10 -0800

    Add most of the tiles

--- src/Debug/Terraine.hs
+++ src/Debug/Terraine.hs
@@ -4,16 +4,18 @@ module Debug.Terraine
 
 import Graphics.Gloss (Picture, pictures, translate)
 import GameObjects.Terraine
-import GameObjects.Sprite
+import GameObjects.Sprite hiding (Sprite(..))
+import qualified GameObjects.Sprite as S (Sprite(..))
 import Debug.Image (debugBoundingBox)
 
 -- TODO only works for bitmaps
-debugTileBoundingBox :: Sprite -> Picture
-debugTileBoundingBox (Sprite x y tile) = translate x y $ debugBoundingBox tile
+debugTileBoundingBox :: S.Sprite -> Picture
+debugTileBoundingBox (S.Sprite x y tile) = translate x y $ debugBoundingBox tile
 
 debugTerraine :: IO Picture
 debugTerraine = do
   tObj <- terraineObjects
+  tTil <- terraineTiles
   return $ pictures $ map debugAndDrawTile
     [ mkSprite (-600)    300 $ horizontalBridge tObj
     , mkSprite (-600)    100 $ verticalBridge tObj
@@ -29,5 +31,25 @@ debugTerraine = do
     , mkSprite (-490) (-130) $ rock2 tObj
     , mkSprite (-490) (-210) $ rock3 tObj
     , mkSprite (-490) (-290) $ rock4 tObj
+    , mkSprite (-650) (-370) $ bush1 tObj
+    , mkSprite (-570) (-370) $ bush2 tObj
+    , mkSprite (-380)    350 $ rockWallDown tObj
+    , mkSprite (-380)    270 $ rockWallUp tObj
+    , mkSprite (-440)    100 $ rockWallLeft tObj
+    , mkSprite (-360)    100 $ rockWallRight tObj
+    , mkSprite (-410)  (-50) $ rockWallTopLeft tObj
+    , mkSprite (-410) (-130) $ rockWallTopRight tObj
+    , mkSprite (-410) (-210) $ rockWallBottomLeft tObj
+    , mkSprite (-410) (-290) $ rockWallBottomRight tObj
+    , mkSprite (-150)    300 $ roadCrossing tTil
+    , mkSprite (-230)    100 $ roadTopLeft tTil
+    , mkSprite (-230)  (-40) $ roadTopRight tTil
+    , mkSprite (-230) (-180) $ roadBottomLeft tTil
+    , mkSprite (-230) (-320) $ roadBottomRight tTil
+    , mkSprite     20    300 $ roadStrip tTil
+    , mkSprite  (-20)    100 $ roadTopLeftSharp tTil
+    , mkSprite  (-20)  (-40) $ roadTopRightSharp tTil
+    , mkSprite  (-20) (-180) $ roadBottomLeftSharp tTil
+    , mkSprite  (-20) (-320) $ roadBottomRightSharp tTil
     ]
   where debugAndDrawTile = pictures . sequence [debugTileBoundingBox, draw]
--- src/GameObjects/Terraine.hs
+++ src/GameObjects/Terraine.hs
@@ -1,10 +1,11 @@
-{-# LANGUAGE NamedFieldPuns #-}
 module GameObjects.Terraine
   ( cropTile
   , cropTiles
   , readTerraineImage
   , terraineObjects
+  , terraineTiles
   , TerraineObjects(..)
+  , TerraineTiles(..)
   ) where
 
 import ThirdParty.GraphicsGlossJuicy (fromImageRGBA8)
@@ -12,6 +13,26 @@ import Lib.Image (readPngOrError)
 import Codec.Picture (DynamicImage, convertRGBA8)
 import Codec.Picture.Extra (crop)
 import Graphics.Gloss (Picture)
+
+tileWidth :: Int
+tileWidth = 64
+
+tileHeight :: Int
+tileHeight = 64
+
+readTerraineImage :: IO DynamicImage
+readTerraineImage = readPngOrError "assets/grass_tileset.png"
+
+-- x and y are the coordinates of the tile in the tileset
+cropTile :: Int -> Int -> DynamicImage -> Picture
+cropTile x y = fromImageRGBA8 . cropFn . convertRGBA8
+  where cropFn = crop (x * tileWidth) (y * tileHeight) tileWidth tileHeight
+
+-- x and y are the coordinates of the tile in the tileset
+-- w and h are the number of tiles representing width and height
+cropTiles :: Int -> Int -> Int -> Int -> DynamicImage -> Picture
+cropTiles x y w h = fromImageRGBA8 . cropFn . convertRGBA8
+  where cropFn = crop (x * tileWidth) (y * tileHeight) (w * tileWidth) (h * tileHeight)
 
 data TerraineObjects = TerraineObjects
   { horizontalBridge :: Picture
@@ -28,44 +49,73 @@ data TerraineObjects = TerraineObjects
   , rock2 :: Picture
   , rock3 :: Picture
   , rock4 :: Picture
+  , bush1 :: Picture
+  , bush2 :: Picture
+  , rockWallDown :: Picture
+  , rockWallUp :: Picture
+  , rockWallLeft :: Picture
+  , rockWallRight :: Picture
+  , rockWallTopLeft :: Picture
+  , rockWallTopRight :: Picture
+  , rockWallBottomLeft :: Picture
+  , rockWallBottomRight :: Picture
   }
-
-tileWidth :: Int
-tileWidth = 64
-
-tileHeight :: Int
-tileHeight = 64
-
-readTerraineImage :: IO DynamicImage
-readTerraineImage = readPngOrError "assets/Grass Tileset.png"
-
--- x and y are the coordinates of the tile in the tileset
-cropTile :: Int -> Int -> DynamicImage -> Picture
-cropTile x y = fromImageRGBA8 . cropFn . convertRGBA8
-  where cropFn = crop (x * tileWidth) (y * tileHeight) tileWidth tileHeight
-
--- x and y are the coordinates of the tile in the tileset
--- w and h are the number of tiles representing width and height
-cropTiles :: Int -> Int -> Int -> Int -> DynamicImage -> Picture
-cropTiles x y w h = fromImageRGBA8 . cropFn . convertRGBA8
-  where cropFn = crop (x * tileWidth) (y * tileHeight) (w * tileWidth) (h * tileHeight)
 
 terraineObjects :: IO TerraineObjects
 terraineObjects = do
   im <- readTerraineImage
   return $ TerraineObjects
-    { horizontalBridge = cropTiles 7 13 3 2 im
-    , verticalBridge   = cropTiles 10 12 3 3 im
-    , greenTree1       = cropTile 13 6 im
-    , greenTree2       = cropTile 14 6 im
-    , greenTree3       = cropTile 13 7 im
-    , greenTree4       = cropTile 14 7 im
-    , brownTree1       = cropTile 13 9 im
-    , brownTree2       = cropTile 14 9 im
-    , brownTree3       = cropTile 13 10 im
-    , brownTree4       = cropTile 14 10 im
-    , rock1            = cropTile 13 12 im
-    , rock2            = cropTile 14 12 im
-    , rock3            = cropTile 13 13 im
-    , rock4            = cropTile 14 13 im
+    { horizontalBridge    = cropTiles 7 13 3 2 im
+    , verticalBridge      = cropTiles 10 12 3 3 im
+    , greenTree1          = cropTile 13 6 im
+    , greenTree2          = cropTile 14 6 im
+    , greenTree3          = cropTile 13 7 im
+    , greenTree4          = cropTile 14 7 im
+    , brownTree1          = cropTile 13 9 im
+    , brownTree2          = cropTile 14 9 im
+    , brownTree3          = cropTile 13 10 im
+    , brownTree4          = cropTile 14 10 im
+    , rock1               = cropTile 13 12 im
+    , rock2               = cropTile 14 12 im
+    , rock3               = cropTile 13 13 im
+    , rock4               = cropTile 14 13 im
+    , bush1               = cropTile 11 9 im
+    , bush2               = cropTile 12 9 im
+    , rockWallDown        = cropTiles 8 9 3 1 im
+    , rockWallUp          = cropTiles 8 10 3 1 im
+    , rockWallLeft        = cropTiles 6 10 1 3 im
+    , rockWallRight       = cropTiles 7 10 1 3 im
+    , rockWallTopLeft     = cropTile 8 11 im
+    , rockWallTopRight    = cropTile 9 11 im
+    , rockWallBottomLeft  = cropTile 8 12 im
+    , rockWallBottomRight = cropTile 9 11 im
+    }
+
+data TerraineTiles = TerraineTiles
+  { roadCrossing :: Picture
+  , roadTopLeft :: Picture
+  , roadTopRight :: Picture
+  , roadBottomLeft :: Picture
+  , roadBottomRight :: Picture
+  , roadStrip :: Picture
+  , roadTopLeftSharp :: Picture
+  , roadTopRightSharp :: Picture
+  , roadBottomLeftSharp :: Picture
+  , roadBottomRightSharp :: Picture
+  }
+
+terraineTiles :: IO TerraineTiles
+terraineTiles = do
+  im <- readTerraineImage
+  return $ TerraineTiles
+    { roadCrossing         = cropTiles 1 6 3 3 im
+    , roadTopLeft          = cropTiles 11 1 2 2 im
+    , roadTopRight         = cropTiles 13 1 2 2 im
+    , roadBottomLeft       = cropTiles 11 3 2 2 im
+    , roadBottomRight      = cropTiles 13 3 2 2 im
+    , roadStrip            = cropTiles 9 1 1 3 im
+    , roadTopLeftSharp     = cropTiles 5 1 2 2 im
+    , roadTopRightSharp    = cropTiles 6 1 2 2 im
+    , roadBottomLeftSharp  = cropTiles 5 2 2 2 im
+    , roadBottomRightSharp = cropTiles 6 2 2 2 im
     }
